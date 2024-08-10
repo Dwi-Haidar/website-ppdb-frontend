14ff3ae5bdf17623eb30b14176458eb13fdde4ff
fadhli_fajarsyah <fadhlifajarsyah089@gmail.com>
2024-08-10 21:46:19 +0700

    ppdbonline

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from "./pages/home";
 import Login from "./pages/login";
 import Layouts from "./layouts/layouts";
 import PpdbOnline from "./pages/ppdbOnline";
+import LayoutsAdmin from "./layouts/layoutsAdmin";
+import Dashboard from "./pages/dashboard"
 
 const App = () => {
   return (
@@ -11,10 +13,13 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layouts />}>
-          <Route index element={<Home/ >}/>
+            <Route index element={<Home/ >}/>
             <Route path="/About" element={<About />} />
             <Route path="/login" element={<Login />} />
             <Route path="/ppdb-online" element={<PpdbOnline />} />
+          </Route>
+          <Route path="/admin" element={<LayoutsAdmin />}>
+           <Route index element={<Dashboard />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+
+const dashboard = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-[100vh] ">
+      <p className="text-[50px] font-bold text-green-600">Welcome to dashboard SMPN ISLAM KARYA MUKTI</p>
+    </div>
+  )
+}
+
+export default dashboard
