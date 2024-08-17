e67e0f45d6e11636426bdf84654f6e04cbaadd69
rizki166 <rizkimuhamad953@gmail.com>
2024-08-17 13:20:43 +0700

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ import Dashboard from "./pages/dashboard";
 import DataPpdb from "../src/pages/ppdbData";
 import PpdbOfline from "./pages/ppdbOfline";
 import Pengumuman from "./pages/pengumuman";
-import DataPpdbEdit from "./pages/editKelulusan"; 
+import DataPpdbEdit from "./pages/editKelulusan";
+import Berita from "./pages/berita";
+import ArticlesPage from "./pages/article";
 const App = () => {
   return (
     <div>
@@ -22,6 +24,8 @@ const App = () => {
             <Route path="ppdb-online" element={<PpdbOnline />} />
             <Route path="ppdb-ofline" element={<PpdbOfline />} />
             <Route path="pengumuman" element={<Pengumuman />} />
+            <Route path="berita" element={<Berita />} />
+            {/* <Route path="artikel" element={<ArticlesPage />} /> */}
           </Route>
           <Route path="/admin" element={<LayoutsAdmin />}>
             <Route index element={<Dashboard />} />
