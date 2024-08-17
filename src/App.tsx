1d76bb240c160847227c4812c5e2a63db6fbf05d
fadhli_fajarsyah <fadhlifajarsyah089@gmail.com>
2024-08-17 14:37:04 +0700

    lolgin

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import About from "./pages/about";
 import Home from "./pages/home";
 import Login from "./pages/login";
@@ -6,15 +6,23 @@ import Layouts from "./layouts/layouts";
 import PpdbOnline from "./pages/ppdbOnline";
 import LayoutsAdmin from "./layouts/layoutsAdmin";
 import Dashboard from "./pages/dashboard";
-import DataPpdb from "../src/pages/ppdbData";
+import DataPpdb from "./pages/ppdbData";
 import PpdbOfline from "./pages/ppdbOfline";
 import Pengumuman from "./pages/pengumuman";
 import DataPpdbEdit from "./pages/editKelulusan"; 
+
 const App = () => {
+  const isAuthenticated = !!localStorage.getItem('authToken');
+
+  const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+    return isAuthenticated ? children : <Navigate to="/login" />;
+  };
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          {/* Routes accessible to everyone */}
           <Route path="/" element={<Layouts />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
@@ -23,7 +31,9 @@ const App = () => {
             <Route path="ppdb-ofline" element={<PpdbOfline />} />
             <Route path="pengumuman" element={<Pengumuman />} />
           </Route>
-          <Route path="/admin" element={<LayoutsAdmin />}>
+          
+          {/* Protected Routes */}
+          <Route path="/admin" element={<ProtectedRoute><LayoutsAdmin /></ProtectedRoute>}>
             <Route index element={<Dashboard />} />
             <Route path="ppdb-data" element={<DataPpdb />} />
             <Route path="ppdb-data/edit/:id" element={<DataPpdbEdit />} />
